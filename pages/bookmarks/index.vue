7f6259bd725a10add644104426b303c7472e1a92
Alec Campbell <88142969+22940dev@users.noreply.github.com>
2021-08-11 15:34:11 -0400

    Create index.vue

---
+++ pages/bookmarks/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <div class="prose" m="y-12">
+      <h1>
+        Bookmarks
+      </h1>
+    </div>
+    <BookmarksList />
+  </div>
+</template>
